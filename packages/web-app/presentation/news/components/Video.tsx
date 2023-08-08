3a331541837473021b3fd426adf9a9003b3c400f
tlavrenyuk <obidnyak@me.com>
2023-08-08 11:28:03 +0200

    feat: update web app ui

--- packages/web-app/presentation/news/components/Video.tsx
+++ packages/web-app/presentation/news/components/Video.tsx
@@ -20,21 +20,23 @@ export function Video({ item }: IProps) {
           <h1 className="mb-4 text-2xl font-bold leading-tight text-gray-900 lg:mb-6 lg:text-4xl dark:text-white">
             {item.title}
           </h1>
-          <div className="flex justify-between">
-            <div className="flex gap-x-6">
-              <img
-                className="h-4 w-4 rounded-sm"
-                src={item.publisher.avatarUrl}
-                alt=""
-              />
-              <div>
-                <h3 className="text-base leading-4 tracking-tight text-gray-900">
-                  {item.publisher.title}
-                </h3>
+          {item.publisher && (
+            <div className="flex justify-between">
+              <div className="flex gap-x-6">
+                <img
+                  className="h-4 w-4 rounded-sm"
+                  src={item.publisher.avatarUrl}
+                  alt=""
+                />
+                <div>
+                  <h3 className="text-base leading-4 tracking-tight text-gray-900">
+                    {item.publisher.title}
+                  </h3>
+                </div>
               </div>
+              <p className="text-sm">{item.publishedAtFormatted}</p>
             </div>
-            <p className="text-sm">{item.publishedAtFormatted}</p>
-          </div>
+          )}
         </header>
       </article>
     </main>
